f266d1b437565a887335d50c07d93fd79a67c016
Will Colding <william.colding@gmail.com>
2023-11-21 00:43:23 -0500

    Missed a couple things

--- SWR_AP_Client/SWR.cpp
+++ SWR_AP_Client/SWR.cpp
@@ -6,7 +6,6 @@
 #include <random>
 #include <chrono>
 
-SWRGame::SWRGame()
 namespace SWRGame
 {
 	int baseAddress;
@@ -15,159 +14,159 @@ namespace SWRGame
 
 	void Init()
 	{
-	baseAddress = (int)GetModuleHandleA("SWEP1RCR.EXE");
-	Patches::SetBaseAddress(baseAddress);
+		baseAddress = (int)GetModuleHandleA("SWEP1RCR.EXE");
+		Patches::SetBaseAddress(baseAddress);
 
-	queuedDeaths = 0;
-	requiredPlacement = RacePlacement::First;
+		queuedDeaths = 0;
+		requiredPlacement = RacePlacement::First;
 
-	Log("Star Wars Episode I Racer Archipelago Client started");
-	
-	Log("Applying patch: Limit Available Racers");
-	Patches::LimitAvailableRacers();
-	Log("Applying patch: Disable Pit Droid Shop");
-	Patches::DisablePitDroidShop();
-	Log("Applying patch: Disable Part Degredation");
-	Patches::DisablePartDegradation();
+		Log("Star Wars Episode I Racer Archipelago Client started");
 
-	APSaveData::Init();
+		Log("Applying patch: Limit Available Racers");
+		Patches::LimitAvailableRacers();
+		Log("Applying patch: Disable Pit Droid Shop");
+		Patches::DisablePitDroidShop();
+		Log("Applying patch: Disable Part Degredation");
+		Patches::DisablePartDegradation();
 
-	// Temporary, for testing purposes
-	// Limit character selection to a random racer
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::default_random_engine gen(seed);
-	std::uniform_int_distribution<int> distribution(0, 22);
-	int random = distribution(gen);
-	APSaveData::unlockedRacers = (RacerUnlocks)(1 << random);
-}
+		APSaveData::Init();
+
+		// Temporary, for testing purposes
+		// Limit character selection to a random racer
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+		std::default_random_engine gen(seed);
+		std::uniform_int_distribution<int> distribution(0, 22);
+		int random = distribution(gen);
+		APSaveData::unlockedRacers = (RacerUnlocks)(1 << random);
+	}
 
 	void Update()
-{
-	if (isSaveFileLoaded())
 	{
-		ScanLocationChecks();
+		if (isSaveFileLoaded())
+		{
+			ScanLocationChecks();
+		}
+
+		ProcessDeathQueue();
+
+		Sleep(50);
 	}
-
-	ProcessDeathQueue();
-
-	Sleep(50);
-}
 
 	void Log(const char* format, ...)
-{
-	auto now = std::chrono::system_clock::now();
-	std::string newFormat = std::format("[{0:%T}] {1}\n", now, format);
-	va_list args;
-	va_start(args, format);
-	vprintf(newFormat.c_str(), args);
-	va_end(args);
-}
+	{
+		auto now = std::chrono::system_clock::now();
+		std::string newFormat = std::format("[{0:%T}] {1}\n", now, format);
+		va_list args;
+		va_start(args, format);
+		vprintf(newFormat.c_str(), args);
+		va_end(args);
+	}
 
 	bool isSaveFileLoaded()
-{
-	char* firstChar = (char*)(baseAddress + SAVE_DATA_OFFSET);
-	return firstChar[0] != 0;
-}
+	{
+		char* firstChar = (char*)(baseAddress + SAVE_DATA_OFFSET);
+		return firstChar[0] != 0;
+	}
 
 	bool isPlayerInRace()
-{
-	PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
-	if (playerPodData == nullptr)
+	{
+		PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
+		if (playerPodData == nullptr)
+			return false;
+
+		RaceData* raceData = (RaceData*)(baseAddress + RACE_DATA_OFFSET);
+		if (raceData == nullptr)
+			return false;
+
+		if (raceData->timer > 0.0f)
+			return true;
+
 		return false;
-
-	RaceData* raceData = (RaceData*)(baseAddress + RACE_DATA_OFFSET);
-	if (raceData == nullptr)
-		return false;
-
-	if (raceData->timer > 0.0f)
-		return true;
-
-	return false;
-}
+	}
 
 	bool isPlayerKillable()
-{
-	if (!isPlayerInRace())
-		return false;
+	{
+		if (!isPlayerInRace())
+			return false;
 
-	PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
-	if (playerPodData == nullptr)
-		return false;
+		PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
+		if (playerPodData == nullptr)
+			return false;
 
-	int unkillable = PodStatus::Destroyed | PodStatus::Autopilot | PodStatus::Invincible;
+		int unkillable = PodStatus::Destroyed | PodStatus::Autopilot | PodStatus::Invincible;
 
-	if ((playerPodData->status & unkillable) != 0)
-		return false;
+		if ((playerPodData->status & unkillable) != 0)
+			return false;
 
-	return true;
-}
+		return true;
+	}
 
 	void KillPod()
-{
-	// Function at SWE1RCR.EXE + 0x74970 checks this flag and destroys the pod if it is set
-	PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
-	if (playerPodData == nullptr)
-		return;
+	{
+		// Function at SWE1RCR.EXE + 0x74970 checks this flag and destroys the pod if it is set
+		PodData* playerPodData = *(PodData**)(baseAddress + POD_DATA_PTR_OFFSET);
+		if (playerPodData == nullptr)
+			return;
 
-	playerPodData->status |= PodStatus::Destroyed;
-	queuedDeaths--;
+		playerPodData->status |= PodStatus::Destroyed;
+		queuedDeaths--;
 
-	Log("Killing player");
-	Log("Queued deaths: %i", queuedDeaths);
-}
+		Log("Killing player");
+		Log("Queued deaths: %i", queuedDeaths);
+	}
 
 	void QueueDeath()
-{
-	queuedDeaths++;
+	{
+		queuedDeaths++;
 
-	Log("Deathlink received! Queueing death");
-	Log("Queued deaths: %i", queuedDeaths);
-}
+		Log("Deathlink received! Queueing death");
+		Log("Queued deaths: %i", queuedDeaths);
+	}
 
 	void ScanLocationChecks()
-{
-	SaveData* saveData = (SaveData*)(baseAddress + SAVE_DATA_OFFSET);
-
-	// Race progress
-	if (requiredPlacement == RacePlacement::Fourth)
 	{
-		// Check unlocked courses
+		SaveData* saveData = (SaveData*)(baseAddress + SAVE_DATA_OFFSET);
 
-	}
-	else
-	{
-		// Check placement flags
-		int flag;
-		CourseData* course;
-		for (int i = 0; i < APSaveData::completedCourses.size(); i++)
+		// Race progress
+		if (requiredPlacement == RacePlacement::Fourth)
 		{
-			course = &APSaveData::completedCourses[i];
-			if (course->completed)
-				continue;
+			// Check unlocked courses
 
-			flag = saveData->racePlacements >> (course->slot * 2);
-			flag &= 0x03;
-
-			if (flag >= (int)requiredPlacement)
+		}
+		else
+		{
+			// Check placement flags
+			int flag;
+			CourseData* course;
+			for (int i = 0; i < APSaveData::completedCourses.size(); i++)
 			{
-				course->completed = true;
+				course = &APSaveData::completedCourses[i];
+				if (course->completed)
+					continue;
 
-				// Notify of location check
-				Log("Location checked: %s", course->name.c_str());
+				flag = saveData->racePlacements >> (course->slot * 2);
+				flag &= 0x03;
+
+				if (flag >= (int)requiredPlacement)
+				{
+					course->completed = true;
+
+					// Notify of location check
+					Log("Location checked: %s", course->name.c_str());
+				}
 			}
 		}
+
+		// Watto Shop
+
+		// Junkyard
+
+		// Pit Droid Shop
 	}
 
-	// Watto Shop
-
-	// Junkyard
-
-	// Pit Droid Shop
-}
-
-void SWRGame::ProcessDeathQueue()
-{
-	if ((queuedDeaths > 0) && isPlayerKillable())
-		KillPod();
-}
+	void ProcessDeathQueue()
+	{
+		if ((queuedDeaths > 0) && isPlayerKillable())
+			KillPod();
+	}
 }
