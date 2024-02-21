e191462fa03771e7f2ed6a209f0cf8f075cc7ce3
Will Colding <william.colding@gmail.com>
2024-02-21 17:22:22 -0500

    Let players keep farmed money Only add new money items, ignore resent

--- SWR_AP_Client/SWR.cpp
+++ SWR_AP_Client/SWR.cpp
@@ -308,7 +308,7 @@ namespace SWRGame
 	{
 		// Reset values of progressive/stackable items (except circuit pass)
 		// AP will send items on connect so we will recalculate from the base values
-		swrSaveData->money = 400;
+		//swrSaveData->money = 400;
 		swrSaveData->pitDroids = 1;
 		memset(&swrSaveData->parts, 0, 7);
 
@@ -457,7 +457,8 @@ namespace SWRGame
 				GiveCircuitPass(itemInfo.param1);
 				break;
 			case ItemType::Money:
-				GiveMoney(itemInfo.param1);
+				if (item.notify)
+					GiveMoney(itemInfo.param1);
 				break;
 			default:
 				break;
