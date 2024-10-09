d0c1d242ed48a0ed20e97b873b3a21e6925b51c0
Will Colding <william.colding@gmail.com>
2024-10-09 12:04:15 -0400

    "Check Releases" button added to login dialog

--- SWR_AP_Client/resource.h
+++ SWR_AP_Client/resource.h
@@ -8,14 +8,15 @@
 #define IDC_SERVER_BOX                  1005
 #define IDC_PLAYER_BOX                  1006
 #define IDC_PASSWORD_BOX                1007
+#define IDC_CHECK_RELEASES              1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
